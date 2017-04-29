20aa84cd077c373ef4fdf16834a12a6df21213b3
hehaiyi <355152171@qq.com>
2017-04-29 16:58:45 +0800

    Button

--- firmware_mpg_common/application/user_app1.c
+++ firmware_mpg_common/application/user_app1.c
@@ -43,7 +43,7 @@ All Global variable names shall start with "G_UserApp1"
 /* New variables */
 volatile u32 G_u32UserApp1Flags;                       /* Global state flags */
 
-
+u8 u8ButtonValue;
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* Existing variables (defined in other files -- should all contain the "extern" keyword) */
 extern volatile u32 G_u32SystemFlags;                  /* From main.c */
@@ -87,10 +87,18 @@ Promises:
 */
 void UserApp1Initialize(void)
 {
- 
+    LedOff(CYAN);
+    LedOff(GREEN);
+    LedOff(YELLOW);
+    LedOff(ORANGE);
+    LedOff(WHITE);
+    LedOff(ORANGE);
+    LedOff(RED);
+    LedOff(CYAN);
+    
   /* If good initialization, set state to Idle */
   if( 1 )
-  {    HEARTBEAT_ON();
+  {    
     UserApp1_StateMachine = UserApp1SM_Idle;
   }
   else
@@ -134,66 +142,167 @@ State Machine Function Definitions
 
 /*-------------------------------------------------------------------------------------------------------------------*/
 /* Wait for ??? */
-static void UserApp1SM_Idle(void)
-{
 
-   static u32 COUNTER_LIMIT_MS=480;    //�趨led��ʼ����
-   static u32 u32Counter=0;
-   static bool blight=FALSE;            //�����Ƿ�����
-   static u16 u6cycle=2000;                   //2S����
-   static bool bswitch=FALSE;           //�л�����������СledƵ��
   
-   u6cycle--;
-    
-   
-   if(u6cycle>=0)
-   {   
-     static u8 Count=5;                  //��ʼ����Ҫ�任�Ĵ���
-     u32Counter++;
-     if(bswitch==FALSE&&u6cycle==0)         //����任����
-     {
-     COUNTER_LIMIT_MS=COUNTER_LIMIT_MS/2;    //Ƶ�ʱ�Ϊԭ��һ��
-      Count--;
-        if( Count==0)
-          {
-            bswitch=!bswitch;
-          }
-     u6cycle=2000;
-     u32Counter=0;
-     }
-     
-   
-      if(bswitch==TRUE&&u6cycle==0)         //����任����
-      {
-        Count++;
-       if( Count==5)
-       {
-         bswitch=!bswitch;
-       }
-      u6cycle=2000;
-      u32Counter=0;
-      }
-   
- /* С�Ƶ�����*/ 
-  if(u32Counter==COUNTER_LIMIT_MS)
-   {   
-   
-     u32Counter=0;
-      if(blight)
-         {
-        HEARTBEAT_ON();
-     
-         }
-      else
-       {
-         HEARTBEAT_OFF();
-     
-       }
-     
-       blight=!blight;
-   }
+static u8 GetButton(void)
+{  u8 u8ButtonValue;
+    if(IsButtonPressed(BUTTON0))
+    { 
+      LedOn(RED);
+      LedToggle(RED);
+      u8ButtonValue=0;
+    }
+    if(IsButtonPressed(BUTTON1))
+    { 
+      LedOn(RED);
+      LedToggle(RED);
+      u8ButtonValue=1;
+    }
+    if(IsButtonPressed(BUTTON2))
+    {
+      LedOn(RED);
+      LedToggle(RED);
+      u8ButtonValue=2;
+    }
+    if(IsButtonPressed(BUTTON3))   
+    {
+      LedOn(RED);
+      LedToggle(RED);
+       u8ButtonValue=3;    
+    }
+    return u8ButtonValue;
+}
 
+
+static void UserApp1SM_Idle(void)
+{   u8 u8Counter=0;
+    u8 u8InputPassWord[]={0,0,0,0,0,0};
+    u8 u8RealPassWord[]={2,2,3,3,4,4};
+    u8 u8ButtonValue1;
+    u8 u8Index;
+    static  u8 u8isPassword=1;
+    
+    GetButton();
+    u8ButtonValue1=GetButton();
+    u8InputPassWord[u8Counter]=u8ButtonValue1;
+    u8Counter++;    
+   
+    if(u8Counter==6) 
+   {     
+     for(u8Index=0;u8Index<6;u8Index++)
+     {
+       if(u8InputPassWord[u8Index]!=u8RealPassWord[u8Index])
+       {
+          u8isPassword=0;
+          break;
+         
+       }
+     }
+       if(u8isPassword) 
+          {
+            LedOn(GREEN);
+          }
+          else
+           {
+           LedOn(WHITE);
+           }
+        
+       u8Counter=0;
+     
    }
+  
+  
+  
+  
+  
+  
+  
+  
+ /* static bool blight=TRUE;
+       if(WasButtonPressed(BUTTON0))
+      {   ButtonAcknowledge(BUTTON0);
+        if(blight)
+          { 
+            LedOn(BLUE);
+          }
+          else
+          {
+            LedOff(BLUE);
+          }
+      
+          blight=!blight;
+        
+      }
+  */
+  
+     
+ /* u8 u8ButtonValue;
+    if(IsButtonPressed(BUTTON0))
+    {
+      u8ButtonValue=0;
+    }
+    if(IsButtonPressed(BUTTON1))
+    {
+      u8ButtonValue=1;
+    }
+    if(IsButtonPressed(BUTTON2))
+    {
+      u8ButtonValue=2;
+    }
+    if(IsButtonPressed(BUTTON3))   
+    {
+       u8ButtonValue=3;    
+    }
+    
+  
+    GetButton();
+    switch(u8ButtonValue)
+    {  case 0:
+         LedOn(WHITE);
+         break;
+       case 1:
+         LedOn(PURPLE);
+         break;
+       case 2:
+         LedOn(RED);
+         break;
+       case 3:
+         LedOff(WHITE);
+         LedOff(RED);
+         LedOff(PURPLE);
+         break;
+    */
+
+  
+  
+  
+ /* 
+    if(WasButtonPressed(BUTTON0))
+    {
+      ButtonAcknowledge(BUTTON0);
+      LedOn(WHITE);
+    }
+    
+    if(WasButtonPressed(BUTTON1))
+    { 
+      ButtonAcknowledge(BUTTON1);
+      LedOn(PURPLE);
+    }
+    
+    if(WasButtonPressed(BUTTON2))
+    { 
+    ButtonAcknowledge(BUTTON2);  
+    LedOn(RED);
+    }
+    
+    if(WasButtonPressed(BUTTON3))
+    {
+    ButtonAcknowledge(BUTTON3);
+    LedOff(WHITE);
+    LedOff(RED);
+    LedOff(PURPLE);
+    }
+   */
 } /* end UserApp1SM_Idle() */
     
 #if 0
